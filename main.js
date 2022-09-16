33f876839e40d36009f3c179049ad726160aef02
APRO100 <106184498+APRO100@users.noreply.github.com>
2022-09-16 20:09:36 +0500

    Add files via upload
    Добавил страницу заказа.

---
+++ main.js
@@ -0,0 +1,53 @@
+const products = document.querySelectorAll(".item");
+const totalSum = document.querySelector(".sum");
+totalSum.textContent = 0;
+
+function totalCounting() {
+  orderSum = 0;
+  for (const product of products) {
+    let checkboxElem = product.querySelector('input[type="checkbox"]');
+    let amountElem = product.querySelector('input[type="number"]');
+    if (checkboxElem.checked) {
+      if (amountElem.value <= 0 || amountElem.value > 100) {
+        amountElem.value = 1;
+      }
+      orderSum +=
+        parseInt(checkboxElem.dataset.price) * parseInt(amountElem.value);
+    } else {
+      amountElem.value = 0;
+    }
+    totalSum.textContent = orderSum;
+  }
+}
+
+function orderConfirmation() {
+  let clientName = document.querySelector(".name").value;
+  let clientSurname = document.querySelector(".surname").value;
+  orderList = [];
+  for (const product of products) {
+    let checkboxElem = product.querySelector('input[type="checkbox"]');
+    let amountElem = product.querySelector('input[type="number"]');
+    if (checkboxElem.checked) {
+      orderSumPosition =
+        parseInt(checkboxElem.dataset.price) * parseInt(amountElem.value);
+      productName = checkboxElem.dataset.goods;
+      position = `${productName} - ${amountElem.value} шт. = ${orderSumPosition} р.`;
+      orderList.push(position);
+    }
+  }
+  if (clientName == "" && clientSurname == "") {
+    orderer = "";
+  } else {
+    orderer = `Заказчик: ${clientName} ${clientSurname}\n\n`;
+  }
+
+  if (orderList == false) {
+    alert("Ничего не выбрано!");
+  } else {
+    alert(
+      `${orderer}Ваш заказ:\n${orderList.join("\n")}\n\nИтого: ${
+        totalSum.textContent
+      } руб.`
+    );
+  }
+}
